7814d1dc7a6607cb012e86c50c40da92f6d492d2
Tyler Dance <tylerdance@Tylers-iMac.fios-router.home>
2020-12-11 11:09:48 -0500

    routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import Procedures from './components/Procedures';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter >
+      <div className="App">
+        <Route path="/" exact component={Home} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/procedures" component={Procedures} />
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Procedures.js
@@ -0,0 +1,11 @@
+function Procedures() {
+
+    return(
+        <div>
+            Procedures
+        </div>
+    )
+}
+
+
+export default Procedures;
